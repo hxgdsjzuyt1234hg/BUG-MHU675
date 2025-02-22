77ce08fc92a69a704c72ca04445185090f00fe54
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-02-22 16:22:42 +0530

    Add files via upload

---
+++ set.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ChannelUsername: "@ekalcrash",
+    githubToken: '',
+    githubRepo: '',
+    ownerId: '7728085772',
+    githubResellerFilePath: 'resellers.json',
+    githubFilePath: 'trashissue.json'
+};
+
