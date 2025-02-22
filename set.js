993239f4ff64dcd6eac79fe17c2340a8b61a07e3
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-02-22 16:22:05 +0530

    Delete set.js

--- set.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ChannelUsername: "@rainonetime",
-    githubToken: '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',
-    githubRepo: 'ZephyrineNasution/databasex',
-    ownerId: '7430156268',
-    githubResellerFilePath: 'resellers.json',
-    githubFilePath: 'trashissue.json'
-};
-
